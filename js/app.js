5df8b121e3c3bb4203cc135b23a7ab06c4649624
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-02-17 14:20:10 +0200

    add 5prompt

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+var pubg = prompt('do you like pubg').toLowerCase()
+
+while(pubg!='yes'&& pubg!='no'){
+    pubg = prompt('your answer should be yes or no (do you like pubg)').toLowerCase()
+}
+if(pubg === 'yes'){
+    alert('me too :)')
+}else{
+    alert('unfortunately i like it')
+}
+var age = prompt('are you younger then 25').toLowerCase()
+
+while(age!='yes'&& age!='no'){
+    age = prompt('your answer should be yes or no (do you like pubg)').toLowerCase()
+}
+if(age === 'yes'){
+    alert('me too :)')
+}else{
+    alert('you are bigger then me')
+}
+var food = prompt('do you like fast food').toLowerCase()
+
+while(food!='yes'&& food!='no'){
+    food = prompt('your answer should be yes or no (do you like pubg)').toLowerCase()
+}
+if(food === 'yes'){
+    alert('me too :)')
+}else{
+    alert('unfortunately i like it')
+}
+var job = prompt('are you in computer field').toLowerCase()
+
+while(job!='yes'&& job!='no'){
+    job = prompt('your answer should be yes or no (do you like pubg)').toLowerCase()
+}
+if(job === 'yes'){
+    alert('me too :)')
+}else{
+    alert('good job, I am develober')
+}
+var read = prompt('do you like reading').toLowerCase()
+
+while(read!='yes'&& read!='no'){
+    read = prompt('your answer should be yes or no (do you like pubg)').toLowerCase()
+}
+if(read === 'yes'){
+    alert('me too :)')
+}else{
+    alert('i like it its good you should try it')
+}
