c1c945df797bd124875fcc2abc291a20a23ba065
YazanAlaiwah <eng.yazanalaiwah@gmail.com>
2020-02-18 10:22:41 +0200

    fix syntax

--- js/app.js
+++ js/app.js
@@ -1,61 +1,61 @@
-"use strict";
-var pubg = prompt("do you like pubg").toLowerCase();
+'use strict';
+var pubg = prompt('do you like pubg').toLowerCase();
 
-while (pubg != "yes" && pubg != "no" &&pubg != "y"&&pubg != "n") {
+while (pubg != 'yes' && pubg != 'no' && pubg != 'y' && pubg != 'n') {
   pubg = prompt(
-    "your answer should be yes or no (do you like pubg)"
+    'your answer should be yes or no (do you like pubg)'
   ).toLowerCase();
 }
-if (pubg === "yes"||pubg === "y") {
-  alert("me too :)");
+if (pubg === 'yes' || pubg === 'y') {
+  alert('me too :)');
 } else {
-  alert("unfortunately i like it");
+  alert('unfortunately i like it');
 }
-var age = prompt("are you younger then 25").toLowerCase();
+var age = prompt('are you younger then 25').toLowerCase();
 
-while (age != "yes" && age != "no"&&age != "y" && age != "n") {
+while (age != 'yes' && age != 'no' && age != 'y' && age != 'n') {
   age = prompt(
-    "your answer should be yes or no (are you younger then 25)"
+    'your answer should be yes or no (are you younger then 25)'
   ).toLowerCase();
 }
-if (age === "yes"||age === "y") {
-  alert("me too :)");
+if (age === 'yes' || age === 'y') {
+  alert('me too :)');
 } else {
-  alert("you are bigger then me");
+  alert('you are bigger then me');
 }
-var food = prompt("do you like fast food").toLowerCase();
+var food = prompt('do you like fast food').toLowerCase();
 
-while (food != "yes" && food != "no"&&food != "y" && food != "n") {
+while (food != 'yes' && food != 'no' && food != 'y' && food != 'n') {
   food = prompt(
-    "your answer should be yes or no (do you like fast food)"
+    'your answer should be yes or no (do you like fast food)'
   ).toLowerCase();
 }
-if (food === "yes"||food === "y") {
-  alert("me too :)");
+if (food === 'yes' || food === 'y') {
+  alert('me too :)');
 } else {
-  alert("unfortunately i like it");
+  alert('unfortunately i like it');
 }
-var job = prompt("are you in computer field").toLowerCase();
+var job = prompt('are you in computer field').toLowerCase();
 
-while (job != "yes" && job != "no"&&job != "y" && job != "n") {
+while (job != 'yes' && job != 'no' && job != 'y' && job != 'n') {
   job = prompt(
-    "your answer should be yes or no (are you in computer field)"
+    'your answer should be yes or no (are you in computer field)'
   ).toLowerCase();
 }
-if (job === "yes"||job === "y") {
-  alert("me too :)");
+if (job === 'yes' || job === 'y') {
+  alert('me too :)');
 } else {
-  alert("good job, I am developer");
+  alert('good job, I am developer');
 }
-var read = prompt("do you like reading").toLowerCase();
+var read = prompt('do you like reading').toLowerCase();
 
-while (read != "yes" && read != "no"&&read != "y" && read != "n") {
+while (read != 'yes' && read != 'no' && read != 'y' && read != 'n') {
   read = prompt(
-    "your answer should be yes or no (do you like reading)"
+    'your answer should be yes or no (do you like reading)'
   ).toLowerCase();
 }
-if (read === "yes"||read === "y") {
-  alert("me too :)");
+if (read === 'yes' || read === 'y') {
+  alert('me too :)');
 } else {
-  alert("i like it its good you should try it");
+  alert('i like it its good you should try it');
 }
